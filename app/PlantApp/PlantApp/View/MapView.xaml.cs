3cd0810de7d9193c37ed39c7ebc1befb2c790bb2
Rasmus Rasmussen <rasmus@netscore.dk>
2021-03-17 20:56:49 +0100

    Mapview

--- app/PlantApp/PlantApp/View/MapView.xaml.cs
+++ app/PlantApp/PlantApp/View/MapView.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using RateABeer.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace PlantApp.View
@@ -12,9 +14,26 @@ namespace PlantApp.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapView : ContentPage
     {
+
+
         public MapView()
         {
+            GetLocation();
             InitializeComponent();
+
+        }
+
+        async void GetLocation()
+        {
+            var position = await Geolocation.GetLocationAsync();
+            map.MoveToRegion(new MapSpan(new Position(position.Latitude, position.Longitude), 0.01, 0.01));
+
+        }
+
+        private void Map_MapClicked(object sender, Xamarin.Forms.Maps.MapClickedEventArgs e)
+        {
+            Console.WriteLine($"MapClick: {e.Position.Latitude}, {e.Position.Longitude}");
+
         }
     }
 }
