f26b13a983194547ec55f899a46c02429cf63b69
kasdin <kasperdinsen@gmail.com>
2021-03-17 09:11:04 +0100

    App

---
+++ App/PlantApp/PlantApp/Model/SoilHumidity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantApp.Model
+{
+    class SoilHumidity
+    {
+        private int soilId;
+        private string humidity;
+        private DateTime date;
+
+        public int SoilId { get => soilId; set => soilId = value; }
+        public string Humidity { get => humidity; set => humidity = value; }
+        public DateTime Date { get => date; set => date = value; }
+    }
+}
---
+++ App/PlantApp/PlantApp/ViewModel/PlantController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PlantApp.Model;
+
+namespace PlantApp.ViewModel
+{
+    class PlantController
+    {
+
+        private List<Plant> plantsList = new List<Plant>();
+
+        public List<Plant> GenerateCustomerListAsync()
+        {
+
+            string test;
+            var listOfBtn = new List<Plant>();
+            Plant plante; 
+
+            IEnumerable<Plant> plantList = null;
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://plantprojectapi.azurewebsites.net/");
+            //client.DefaultRequestHeaders.Add("appkey", "myapp_key");
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response1 = client.GetAsync("plant").Result;
+            Console.WriteLine("Besked fra server " + response1.StatusCode);
+            if (response1.IsSuccessStatusCode)
+            {
+                var plants = response1.Content.ReadAsAsync<IEnumerable<Plant>>().Result;
+                foreach (var item in plants)
+                {
+                    Console.WriteLine(item.name);
+                    listOfBtn.Add(item);
+                }
+            }
+            else
+            {
+                //Console.WriteLine("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+
+
+            return listOfBtn;
+        }
+
+        public async Task<Plant> GetProductAsync()
+        {
+            HttpClient client = new HttpClient();
+            Plant product = null;
+            HttpResponseMessage response = await client.GetAsync("https://plantprojectapi.azurewebsites.net/plant");
+            if (response.IsSuccessStatusCode)
+            {
+                
+                product = await response.Content.ReadAsAsync<Plant>();
+                Console.WriteLine("Det virker " + product);
+            }
+            return product;
+        }
+    }
+}
